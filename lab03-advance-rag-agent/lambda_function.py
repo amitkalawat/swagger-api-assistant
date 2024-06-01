d4f0f7991cbc2c27d026ef4b70b776f7c244e3c9
james <jingwu@amazon.com>
2024-06-01 15:41:08 +0000

    add documentation

--- lab03-advance-rag-agent/lambda_function.py
+++ lab03-advance-rag-agent/lambda_function.py
@@ -1,6 +1,67 @@
 import json
 import uuid
 import boto3
+from botocore.config import Config
+
+boto_config = Config(
+        connect_timeout=1, read_timeout=300,
+        retries={'max_attempts': 1})
+
+boto_session = boto3.Session()
+
+bedrock_runtime = boto_session.client(
+    service_name="bedrock-runtime",
+    config=boto_config
+)
+
+code_generation = """
+Your task is to generate a code snippet based on a given OpenAPI YAML file and a user query. The
+YAML file will be provided in the following format:
+
+<YAML_FILE>
+{YAML_FILE}
+</YAML_FILE>
+
+The user query will be provided like this:
+
+<USER_QUERY>
+{USER_QUERY}
+</USER_QUERY>
+
+To complete this task, follow these steps:
+
+1. Carefully read and understand the user query. Identify the specific API endpoint, HTTP method,
+and any required parameters or request body mentioned in the query.
+
+2. Analyze the provided YAML file and locate the relevant section that corresponds to the user's
+query. The YAML file follows the OpenAPI specification and contains information about the API's
+endpoints, methods, parameters, request/response bodies, etc.
+
+3. Once you have identified the relevant section in the YAML file, extract the necessary information
+to construct the code snippet, such as the endpoint URL, HTTP method, required parameters, and
+request body structure (if applicable).
+
+4. Use the extracted information to generate the code snippet in the programming language specified
+by the user (e.g., Python, JavaScript, etc.). The code snippet should include the necessary
+imports/libraries, the API endpoint URL, the HTTP method, any required parameters, and the request
+body (if applicable).
+
+5. Format the generated code snippet using markdown code blocks, like this:
+
+```<programming_language>
+<code_snippet>
+```
+
+This will allow the user to easily copy and paste the code snippet.
+
+6. If the user's query cannot be fulfilled based on the provided YAML file, or if there is any
+ambiguity or missing information, explain the issue politely and ask for clarification from the
+user.
+
+Remember, your goal is to generate a code snippet that accurately reflects the user's query based on
+the information provided in the OpenAPI YAML file. Do not include any additional functionality or
+code beyond what is necessary to fulfill the user's request.
+"""
 
 def get_named_parameter(event, name):
     """
@@ -18,29 +79,40 @@ def get_uml_diagram(yml_code):
     return {"imageUrl":"s3://test-bucket/prefix/image.jpg"}
 
 
-def get_unit_test_code(yml_code):
+def get_unit_test_code(yml_code, query):
     """
     Generate unit test code to invoke an API
     
     """
+    content = []
+
+    prompt = code_generation.replace("{YAML_FILE}",yml_code)
+    prompt = prompt.replace("{USER_QUERY}",query)
+
+    query_obj = {"type": "text", "text": prompt}
+
+    content.append(query_obj)
+
+    body = json.dumps(
+        {
+            "anthropic_version": "bedrock-2023-05-31",
+            "max_tokens": 4096,
+            "messages": [
+                {
+                    "role": "user",
+                    "content": content,
+                }
+            ],
+        }
+    )
     
-    codeBody = """
-    Here is the unit test code:
-    ```python
-    import requests
+    response = bedrock_runtime.invoke_model(
+        modelId="anthropic.claude-3-sonnet-20240229-v1:0",
+        body=body)
     
-    url = "http://petstore.swagger.io/v2/pet"
+    response_body = json.loads(response.get("body").read())
     
-    payload = { "name": "Buddy", "photoUrls": [ "http://pet-images.com/buddy.jpg" ], "category": { "id": 1, "name": "Dogs" }, "status": "available" } 
-    headers = { 'Content-Type': 'application/json' }
-    
-    response = requests.request("POST", url, headers=headers, json=payload)
-    
-    print(response.text)
-    ```
-    """
-    
-    return {"codeBody": codeBody}
+    return {"codeBody": response_body["content"][0]["text"]}
 
 def lambda_handler(event, context):
     # get the action group used during the invocation of the lambda function
@@ -62,9 +134,10 @@ def lambda_handler(event, context):
 
     elif function == 'get_unit_test_code':
         yml_code = get_named_parameter(event, "yml_body")
+        user_query = get_named_parameter(event, "user_query")
 
         if yml_code:
-            response = get_unit_test_code(yml_code)
+            response = get_unit_test_code(yml_code, user_query)
             responseBody = {'TEXT': {'body': json.dumps(response)}}
         else:
             responseBody = {'TEXT': {'body': 'Missing YML code.'}}
